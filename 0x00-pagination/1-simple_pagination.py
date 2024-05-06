ea216310de4c82662a8aad6da255c40f5211618e
Samuel Amihere <amisam2000@gmail.com>
2024-05-07 05:20:45 +0800

    Pagination: Simple helper function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,12 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
-            Returns the appropriate page of the dataset
-            """
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        """
+        Returns the appropriate page of the dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            start, end = index_range(page, page_size)
-            data = self.dataset()
-            return data[start:end]
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        return data[start:end]
