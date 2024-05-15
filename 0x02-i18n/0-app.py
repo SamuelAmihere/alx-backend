d8bc819989759dae93bf4af59e050cede0cbfd3b
Samuel Amihere <amisam2000@gmail.com>
2024-05-15 12:46:56 +0800

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 """
-
+Create a single / route
 """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    """
+    home page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="localhost", port=5000)
