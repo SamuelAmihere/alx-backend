cc490c40f5278d3fa2fd4bba1b09ecd8d47db396
Samuel Amihere <amisam2000@gmail.com>
2024-06-13 06:26:14 +0800

    set data

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,26 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  console.log(await getAsync(schoolName));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
